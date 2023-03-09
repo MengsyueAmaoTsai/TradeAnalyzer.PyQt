efac56871092c1e20b0955b3ce4fd53df835c5bc
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-03-09 22:58:05 +0800

    fix: Set default end date to 23/2/24

--- src/trade_analyzer_desktop/qt/upload_backtest_report_dialog.py
+++ src/trade_analyzer_desktop/qt/upload_backtest_report_dialog.py
@@ -76,7 +76,7 @@ class UploadBacktestReportDialog(QDialog):
 
         # Default Data
         self.__start_date_input.setDate(QDate(2020, 1, 1))
-        self.__end_date_input.setDate(QDate.currentDate())
+        self.__end_date_input.setDate(QDate(2023, 2, 24))
 
         self.__orders_from_file: List[Order] = TextFileOrderProvider.read(path)
 
